d34a10eac0f3f2beb4a654e5d9dcfd923d442635
Thomas <thomas@Thomass-MacBook-Air.local>
2018-10-23 15:02:53 -0700

    finished debugging

--- stage-4/src/redux/reducer.js
+++ stage-4/src/redux/reducer.js
@@ -13,7 +13,7 @@ let initialState = {
 }
 
 // Reducer
-export default function reducer(state, action) {
+export default function reducer(state=initialState, action) {
     switch(action.type) {
         case GET_ALL_PRODUCTS_PENDING:
             return Object.assign({}, state, {loading: true});
@@ -26,8 +26,8 @@ export default function reducer(state, action) {
 
         case REMOVE_FROM_SHOPPING_CART:
             let newArray = state.shoppingCart.slice();
-            newArray.splice(action.index, 1);
-            return Object.assign({}, {shoppingCart: newArray});
+            newArray.splice(action.payload, 1);
+            return Object.assign({}, state, {shoppingCart: newArray});
             
         default:
             return state;
